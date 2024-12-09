0079d31bbe0559a0d5e70a41fc4b5faf11e32877
Arun Choudhary <arundatika2002@gmail.com>
2024-12-09 14:16:52 +0530

    assignment of 7_dec

---
+++ basic/Friend_name.java
@@ -0,0 +1,31 @@
+public class Friend_name {
+    public static void main(String[] args) {
+
+        System.out.println("Friend's name");
+        System.out.println("Aditya");
+        System.out.println("Karan");
+        System.out.println("Arnav");
+        System.out.println("Shivam");
+        System.out.println("abhishek");
+        
+        System.out.println();
+        System.out.println();
+
+        System.out.println("Favriout food name");
+        System.out.println("Dosa");
+        System.out.println("Chole ");
+        System.out.println("Vada");
+        System.out.println("Pav");
+        System.out.println("Rice");
+
+        System.out.println();
+        System.out.println();
+
+        System.out.println("Name of city's");
+        System.out.println("Jaipur");
+        System.out.println("Delhi ");
+        System.out.println("Agra");
+        System.out.println("Lucknow");
+        System.out.println("Mumbai");
+    }
+}
