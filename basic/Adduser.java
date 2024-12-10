23748ff8e749508b40079fb8a198977e88dda71e
Arun Choudhary <arundatika2002@gmail.com>
2024-12-10 13:00:05 +0530

    Sum using user input

---
+++ basic/Adduser.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Adduser {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the numbers for addition:");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = a+b;
+        System.out.println("Sum of "+a+" and "+b+" is "+c);
+    }
+}
