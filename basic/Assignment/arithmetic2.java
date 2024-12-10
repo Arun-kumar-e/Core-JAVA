6a09eef67945ed807308db530bcb304bec480317
Arun Choudhary <arundatika2002@gmail.com>
2024-12-10 15:07:25 +0530

    arithmetic2

---
+++ basic/Assignment/arithmetic2.java
@@ -0,0 +1,10 @@
+public class arithmetic2 {
+    
+    public static void main(String[] args) {
+
+        int a = 234 ;
+        int b =  55;    
+        int c = a - b;
+        System.out.println("Subtraction of " + a + " and " + b + " is " + c);
+    }
+}
