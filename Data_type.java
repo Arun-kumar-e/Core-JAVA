1d18d6f5d1f4ae02ed9a2febfa2662a4cf4c1ca8
Arun Choudhary <arundatika2002@gmail.com>
2024-12-10 14:30:48 +0530

    Initial commit

---
+++ Data_type.java
@@ -0,0 +1,38 @@
+public class Data_type {
+    public static void main(String[] args) {
+
+        // data typee :
+
+        // boolean :true ya false
+        boolean a = true;
+        System.out.println("boolean :" + a);
+
+        // char :alphabets
+        char b = 'o';
+        System.out.println("character : " + b);
+
+        // byte :stored 127 to -128(numbers)
+        byte c = 127;
+        System.out.println("byte " + c);
+
+        // short :32 thousand
+        short d = 896;
+        System.out.println("short " + d);
+
+        // int : number
+        int e = 75;
+        System.out.println("the value of e " + e);
+
+        //long :long number
+        long l = 9876543210l;
+        System.out.println("the value of l " + l); 
+
+        //float f :in java decimal value by default stored in double 
+        float f=675.3f;
+        System.out.println("float :"+f);
+
+        //double 
+        double g = 675.3;
+        System.out.println("double :"+g);
+    }
+}
