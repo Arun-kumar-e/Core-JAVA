221ccbb5f3da77143b278d83d2985791a2f3c789
Arun Choudhary <arundatika2002@gmail.com>
2024-12-13 09:00:52 +0530

    Question of advance if If_Else_Advance__Assignment

---
+++ basic/Assignment/12-dec Control Statement Assignment/If_Else_Advance__Assignment/Ques4.java
@@ -0,0 +1,27 @@
+/**
+ * 4)java program to check whether a character is an alphabet or not
+input =a =alpha 
+digit :
+3 =this is a digit
+*/
+import java.util.Scanner;
+
+public class Ques4 {
+    
+    public static void main(String[] args) {
+        
+        Scanner s = new Scanner(System.in);
+
+        char ch = s.next().charAt(0);
+
+        if (ch>='a' && ch<='z') {
+            System.out.println("Alphabet");
+        } else if (ch>1) {
+            System.out.println("Digit");
+        } else {
+            System.out.println("Invalid input");
+        }
+
+        s.close();
+    }
+}
