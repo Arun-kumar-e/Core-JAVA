b35450a18ec5d25b0cc735b85cf91bf990b7c7bd
Arun Choudhary <arundatika2002@gmail.com>
2024-12-16 16:18:27 +0530

    prime number

--- basic/Assignment/Control Statement/Advance_Question/Prime.java
+++ basic/Assignment/Control Statement/Advance_Question/Prime.java
@@ -7,18 +7,19 @@ public class Prime {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number");
         int num = sc.nextInt();
-        int count = 0;
+        
         for (int i = 1; i <= num; i++) {
+            int count = 0;
             for(int j = 1 ; j<= num ; j++) {
-                      }
-            if (num % i == 0) {
-                count++;
+                if (i % j == 0) {
+                    count++;
+                }
             }
+            if (count == 2) {
+                System.out.print(i +" ");
+            }            
         }
-        if (count == 2) {
-            System.out.println("Prime");
-        } else {
-            System.out.println("Not Prime");
-        }
+
+        sc.close();
     }
 }
