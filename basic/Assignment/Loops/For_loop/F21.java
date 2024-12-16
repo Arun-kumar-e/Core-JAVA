3d193d8e7c7a6cae4c891b8ac1418a1d1b84b62f
Arun Choudhary <arundatika2002@gmail.com>
2024-12-16 15:08:16 +0530

    For_loop's Answer

---
+++ basic/Assignment/Loops/For_loop/F21.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class F21 {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the car no.");
+        int a = sc.nextInt();
+
+        int count = 0 , sum =0;
+        for (int i = a; i > 0; i = i/10) {
+            int r = i%10;
+               sum = sum + r;
+               if(r<=0){
+                System.out.println("invalid car code");
+                break;
+               }
+               count++;
+        }
+
+        if (count == 4) {
+            if (sum%3==0 || sum%5==0 || sum%7==0) {
+                System.out.println("Lucky number");
+            }
+            else{
+                System.out.println("It is not a lucky number");
+            }
+        } else {
+            System.out.println("It is not a valid car number");
+            
+        }
+
+        sc.close();
+    }
+    
+}
