912cf5d34228d3d57163d7ab4d03ece9f0720010
Arun Choudhary <arundatika2002@gmail.com>
2024-12-18 14:10:21 +0530

    baasic method

---
+++ basic/Assignment/OOPs/Assignment1/BasicMethod1.java
@@ -0,0 +1,15 @@
+
+class A {
+    void show(){
+        System.out.println("Hello World");
+    }
+}
+public class BasicMethod1 {
+    
+    public static void main(String[] args) {
+        
+        A obj = new A();
+        obj.show();
+
+    }
+}
