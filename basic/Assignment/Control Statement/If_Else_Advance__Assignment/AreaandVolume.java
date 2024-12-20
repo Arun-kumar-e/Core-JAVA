cdafda8e3c621274c8e6bcf4543dea617ea81143
Arun Choudhary <arundatika2002@gmail.com>
2024-12-20 12:45:17 +0530

    Area and volume calculation

---
+++ basic/Assignment/Control Statement/If_Else_Advance__Assignment/AreaandVolume.java
@@ -0,0 +1,64 @@
+/**
+ 3)Check whether the trainge , square , reactanlge (area & volume)
+->input area /volume 
+ area 
+ traingle square rectangle 
+ square 
+ Area of square
+ */
+
+ 
+import java.util.Scanner;
+
+public class AreaandVolume {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("What you want to calculate (Area/Volume)");
+
+        String a = sc.next();
+
+        if (a.equals("area")) {
+
+            System.out.println("Enter shape you want to calculate the area (Triangle/rectangle/square)");
+
+            String s = sc.next();
+            if (s.equals("triangle")) {
+                System.out.println("Area of triangle ");
+            } 
+            else if (s.equals("rectangle")) {
+                System.out.println("Area of rectangle ");
+            }
+             else if (s.equals("square")) {
+                System.out.println("Area of square ");
+            }
+             else {
+                System.out.println("Invalid input");
+            }
+
+        } else if (a.equals("volume")) {
+
+            System.out.println("Enter shape you want to calculate the Volume (Triangle/rectangle/square)");
+
+            String s = sc.next();
+            if (s.equals("triangle")) {
+                System.out.println("Volume of triangle ");
+            }
+             else if (s.equals("rectangle")) {
+                System.out.println("Volume of rectangle ");
+            }
+             else if (s.equals("square")) {
+                System.out.println("Volume of square ");
+            }
+             else {
+                System.out.println("Invalid input");
+            }
+        } else {
+            System.out.println("Invalid input");
+        }
+
+        sc.close();
+    }
+}
