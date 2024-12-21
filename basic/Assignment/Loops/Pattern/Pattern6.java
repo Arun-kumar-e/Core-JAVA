c507215ecefb2eb73229103b40799c2e6bf9bb67
Arun Choudhary <arundatika2002@gmail.com>
2024-12-21 14:52:04 +0530

    Pattern

---
+++ basic/Assignment/Loops/Pattern/Pattern6.java
@@ -0,0 +1,15 @@
+public class Pattern6 {
+    
+    public static void main(String[] args) {
+        
+        for (int i = 1; i <= 5; i++) {
+            int n = 1;
+
+            for (int j = 1; j <= 5; j++) {
+                System.out.print(n);
+                n++;
+            }
+            System.out.println();
+        }
+    }
+}
