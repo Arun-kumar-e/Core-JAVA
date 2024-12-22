ec443c76b58a7ab0412e45f9875755d31b59776c
Arun Choudhary <arundatika2002@gmail.com>
2024-12-22 19:14:15 +0530

    Patterns

--- basic/Assignment/Loops/Pattern/Pattern34.java
+++ basic/Assignment/Loops/Pattern/Pattern34.java
@@ -1,3 +1,15 @@
 public class Pattern34 {
-    
+ 
+    public static void main(String[] args) {
+        
+        int n = 1;
+        for (int i = 1; i <= 5; i++) {
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print((n*n) + " ");
+                n++;
+            }
+            System.out.println();
+        }
+    }
 }
