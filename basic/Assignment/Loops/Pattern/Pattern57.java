75d141d2456eb944485389c4603fb67deb52698a
Arun Choudhary <arundatika2002@gmail.com>
2024-12-22 20:30:23 +0530

    pattern

--- basic/Assignment/Loops/Pattern/Pattern57.java
+++ basic/Assignment/Loops/Pattern/Pattern57.java
@@ -1,3 +1,17 @@
 public class Pattern57 {
     
+    public static void main(String[] args) {
+        
+        for (int i = 1; i <= 5; i++) {
+            
+            for (int j = 1; j <= 5; j++) {
+                if (j == 5-i+1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(j);
+                }
+            }
+            System.out.println();
+        }
+    }
 }
