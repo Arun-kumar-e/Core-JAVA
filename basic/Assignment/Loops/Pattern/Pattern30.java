008f16321757d58826ed017ddddbbe8ed83da312
Arun Choudhary <arundatika2002@gmail.com>
2024-12-22 18:51:35 +0530

    Patterns

--- basic/Assignment/Loops/Pattern/Pattern30.java
+++ basic/Assignment/Loops/Pattern/Pattern30.java
@@ -1,3 +1,17 @@
 public class Pattern30 {
     
+    public static void main(String[] args) {
+        
+        for (int i = 1; i <= 5; i++) {
+
+            for(int j = 1 ; j <= i; j++){
+                System.out.print(j);
+            }
+
+            for (int s = 5 ; s >i ; s--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
