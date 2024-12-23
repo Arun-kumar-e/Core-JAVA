c2cace241ada817e36a7119a9f144e94265c688d
Arun Choudhary <98233935+Arun-kumar-e@users.noreply.github.com>
2024-12-24 00:38:48 +0530

    operation using user Input Instance

--- arithmetic1.java
+++
@@ -1,8 +0,0 @@
-public class arithmetic1 {
-    public static void main(String[] args) {
-        int a = 45;
-        int b = 7;
-        int c = a + b;
-        System.out.println("Sum of " + a + " and " + b + " is " + c);
-    }
-}
