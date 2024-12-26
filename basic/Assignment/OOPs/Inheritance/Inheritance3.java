f8d280f2c9658275d18df20befb2f762b2e6aa76
Arun Choudhary <98233935+Arun-kumar-e@users.noreply.github.com>
2024-12-26 23:54:14 +0530

    default constructor in Single level Inheritance

---
+++ basic/Assignment/OOPs/Inheritance/Inheritance3.java
@@ -0,0 +1,22 @@
+public class Inheritance3 {
+    
+    public static void main(String[] args) {
+        
+     new ault();
+
+    }
+}
+
+class Def{
+
+    Def(){
+        System.out.println("Default Constructor");
+    }
+}
+
+class ault extends Def{
+
+    ault(){
+        System.out.println("Parameterized Constructor");
+    }
+}
