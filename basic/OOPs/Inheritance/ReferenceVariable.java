eeccf561db337ee3d8ac7fa1cf0b4d0c7a093426
Arun Choudhary <98233935+Arun-kumar-e@users.noreply.github.com>
2024-12-27 12:59:00 +0530

    Refrence variable

--- basic/OOPs/Inheritance/ReferenceVariable.java
+++ basic/OOPs/Inheritance/ReferenceVariable.java
@@ -1,11 +1,14 @@
 public class ReferenceVariable {
     
     public static void main(String[] args) {
-        
+
+        // refrence variable only work if Inheritance is there
         // here o is a reference variable of type A and the object is of type B
         A o = new B();
         o.show();
         // o.show1(); // This will give an error because the reference variable is of type A and the object is of type B
+        
+        
     }
 }
 
@@ -18,5 +21,11 @@ class A{
 class B extends A{
     void show1(){
         System.out.println("hello from clsss B");
+    }
+}
+
+class c {
+    void show2(){
+        System.out.println("hello from clsss C");
     }
 }
