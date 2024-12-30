3a7bdfbad7ebfbb7aa36e0e6f81dc99fba94efd2
Arun Choudhary <98233935+Arun-kumar-e@users.noreply.github.com>
2024-12-30 11:42:27 +0530

    Default Constructor Multilevel Inheritance

---
+++ basic/Assignment/OOPs/Inheritance/DefaultConstructorMultilevel.java
@@ -0,0 +1,40 @@
+public class DefaultConstructorMultilevel {
+    
+    public static void main(String[] args) {
+        
+        D d = new D();
+
+    }
+}
+
+class A{
+
+    A(){
+        System.out.println("Class A");
+    }
+}
+
+class B extends A{
+
+    B(){
+        System.out.println("Class B");
+    }
+
+}
+
+class C extends B{
+
+    C(){
+        System.out.println("Class C");
+    }
+
+}
+
+class D extends C {
+
+    D(){
+        System.out.println("Class D");
+    }
+
+}
+
