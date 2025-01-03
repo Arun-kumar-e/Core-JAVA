6530df38a36ef5501b6694ff499ab341bcf5a616
Arun Choudhary <98233935+Arun-kumar-e@users.noreply.github.com>
2025-01-03 15:54:59 +0530

    All Primitive Data type

--- basic/Assignment/OOPs/Polymorphism (method overloading )/MethodOverloading4.java
+++ basic/Assignment/OOPs/Polymorphism (method overloading )/MethodOverloading4.java
@@ -1,43 +1,43 @@
-public class MethodOverloading4 {
+// public class MethodOverloading4 {
     
-    public static void main(String[] args) {
+//     public static void main(String[] args) {
         
 
-    }
-}
+//     }
+// }
 
-class A{
+// class A{
 
-    int display(){
-        int a = 10;
-        int b = 20;
+//     int display(){
+//         int a = 10;
+//         int b = 20;
         
-        System.out.println("Integer value: ");
-        return 0;
-    }
-    
-    float display(){
-        int a = 10;
-        int b = 20;
+//         System.out.println("Integer value: ");
+//         return 0;
+//     }
 
-        System.out.println("Integer value: ");
-        return 0;
-    }
+//     float display(){
+//         int a = 10;
+//         int b = 20;
 
-    long display(){
-        int a = 10;
-        int b = 20;
+//         System.out.println("Integer value: ");
+//         return 0;
+//     }
 
-        System.out.println("Integer value: ");
-        return 0;
-    }
+//     long display(){
+//         int a = 10;
+//         int b = 20;
 
-    char display(){
-        int a = 10;
-        int b = 20;
+//         System.out.println("Integer value: ");
+//         return 0;
+//     }
 
-        System.out.println("Integer value: ");
-        return 0;
-    }
-}
+//     char display(){
+//         int a = 10;
+//         int b = 20;
+
+//         System.out.println("Integer value: ");
+//         return 0;
+//     }
+// }
 
