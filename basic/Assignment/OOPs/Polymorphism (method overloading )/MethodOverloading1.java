67229169cda13387521100dcb820b9b14068bc32
Arun Choudhary <98233935+Arun-kumar-e@users.noreply.github.com>
2025-01-03 11:02:05 +0530

    Method Overloading

---
+++ basic/Assignment/OOPs/Polymorphism (method overloading )/MethodOverloading1.java
@@ -0,0 +1,40 @@
+public class MethodOverloading1 {
+
+    public static void main(String[] args) {
+        
+        A obj = new A();
+        obj.show(10, 20);
+        obj.show(10, 20L);
+        obj.show(10L, 20);
+        obj.show();
+        obj.show(10.5f, 20);
+
+    }
+}
+
+class A{
+    void show(int a , int b){
+        System.out.println("Add " + (a + b));
+    }
+
+    void show(int a , Long b){
+        System.out.println("Sub " + (a - b));
+    }
+
+    void show(Long a , int b){
+        System.out.println("Multi " + (a * b));
+    }
+
+    void show(){
+        int a = 10;
+        int b = 20;
+        System.out.println("Div " + (a / b));
+    }
+
+    void show(float a , int b){
+        float c = a%b;
+        System.out.println("Mod " + c);
+    }
+
+}
+
