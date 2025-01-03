9e3e2bbcd3106888371ea8ed5343a036f2c85880
Arun Choudhary <98233935+Arun-kumar-e@users.noreply.github.com>
2025-01-03 16:15:11 +0530

    number of parameter same but name of variable different

---
+++ basic/Assignment/OOPs/Polymorphism (method overloading )/MethodOverloading6.java
@@ -0,0 +1,28 @@
+public class MethodOverloading6 {
+    
+    public static void main(String[] args) {
+        
+
+    }
+}
+
+class A{
+
+    void display(int a){
+        System.out.println("Integer value: " + a);
+    }
+
+    void display(int b){
+        System.out.println("Float value: " + b);
+    }
+
+    void display(int c){
+        System.out.println("Float value: " + c);
+    }
+
+    void display(int d){
+        System.out.println("Float value: " + d);
+    }
+    
+}
+
