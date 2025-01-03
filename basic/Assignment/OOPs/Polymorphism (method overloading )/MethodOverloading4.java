3712b2c01c4a9b8e04be9e2762ecb097fb78a143
Arun Choudhary <98233935+Arun-kumar-e@users.noreply.github.com>
2025-01-03 15:53:47 +0530

    Method Overloading with different return type

---
+++ basic/Assignment/OOPs/Polymorphism (method overloading )/MethodOverloading4.java
@@ -0,0 +1,43 @@
+public class MethodOverloading4 {
+    
+    public static void main(String[] args) {
+        
+
+    }
+}
+
+class A{
+
+    int display(){
+        int a = 10;
+        int b = 20;
+        
+        System.out.println("Integer value: ");
+        return 0;
+    }
+    
+    float display(){
+        int a = 10;
+        int b = 20;
+
+        System.out.println("Integer value: ");
+        return 0;
+    }
+
+    long display(){
+        int a = 10;
+        int b = 20;
+
+        System.out.println("Integer value: ");
+        return 0;
+    }
+
+    char display(){
+        int a = 10;
+        int b = 20;
+
+        System.out.println("Integer value: ");
+        return 0;
+    }
+}
+
