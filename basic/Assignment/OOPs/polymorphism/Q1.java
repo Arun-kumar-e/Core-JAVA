064cb081e0a5bc922a3125a1683a100c0207274c
Arun Choudhary <98233935+Arun-kumar-e@users.noreply.github.com>
2025-01-03 18:29:53 +0530

    Constructor Overloading

---
+++ basic/Assignment/OOPs/polymorphism/Q1.java
@@ -0,0 +1,22 @@
+public class Q1 {
+
+    public static void main(String[] args) {
+        
+        new A();
+
+    }
+}
+
+class A{
+
+    A(){
+        System.out.println("Constructor Overloading");
+    }
+
+    A(){
+        System.out.println("Constructor Overloading");
+    }
+
+}
+
+
