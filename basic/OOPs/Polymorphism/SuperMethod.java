4b5b451014795d29284dc22d16a6f7e4bd888ee3
Arun Choudhary <98233935+Arun-kumar-e@users.noreply.github.com>
2025-01-04 13:06:54 +0530

    Super Constructor

---
+++ basic/OOPs/Polymorphism/SuperMethod.java
@@ -0,0 +1,21 @@
+public class SuperMethod {
+    
+    public static void main(String[] args) {
+        
+    }
+}
+
+class A{ 
+
+    A(int a){
+        System.out.println(a);
+    }
+}
+
+class B extends A{ 
+    
+    B(){
+        super(10);
+        System.out.println("B");
+    }
+}
