2e70d7d3adeb175701bbc90234c0aa3db87cb38c
Arun Choudhary <98233935+Arun-kumar-e@users.noreply.github.com>
2025-01-06 12:09:21 +0530

    Access Modifier using public in Method

---
+++ basic/OOPs/concepts/AccessModifierPublic.java
@@ -0,0 +1,23 @@
+public class AccessModifierPublic {
+    public static void main(String[] args) {
+        B obj = new B();
+        obj.display();
+        obj.display2();
+    }
+}
+
+class A{
+
+    public void display(){
+        System.out.println("Access Modifier Public");
+    }
+
+}
+
+class B extends A{
+
+    public void display2(){
+        System.out.println("Access Modifier Public");
+    }
+}
+
