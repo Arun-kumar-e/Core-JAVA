6e88eeb13c964f8dc926779bc2a6208b40acd8b7
Arun Choudhary <98233935+Arun-kumar-e@users.noreply.github.com>
2025-01-30 17:31:35 +0530

    Variable

--- basic/OOPs/Interface/Interface3.java
+++ basic/OOPs/Interface/Interface3.java
@@ -1,3 +1,20 @@
 public class Interface3 {
-    
+    public static void main(String[] args) {
+        C obj = new C();
+        obj.show();
+    }
+}
+
+interface A {
+    int a = 12;
+}
+
+interface B extends A {
+    //a = 13;  cannot reassign value to a constant (they are by )
+}
+
+class C implements B {
+    public void show() {
+        System.out.println(A.a);
+    }
 }
