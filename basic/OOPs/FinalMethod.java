e7b6adaa960cc1d4ca4e6585bfa8c04131f59922
Arun Choudhary <98233935+Arun-kumar-e@users.noreply.github.com>
2025-01-30 17:40:16 +0530

    Final in method

---
+++ basic/OOPs/FinalMethod.java
@@ -0,0 +1,18 @@
+public class FinalMethod {
+    public static void main(String[] args) {
+        B obj = new B();
+        obj.show();
+    }
+}
+
+class A{
+    final void show(){
+        System.out.println("A");
+    }
+}
+class B extends A{
+    // error because final method can't be overridden
+    void show(){
+        System.out.println("B");
+    }
+}
