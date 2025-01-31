8b00080f35f4eb5baf65365197dcaa5facf695ea
Arun Choudhary <98233935+Arun-kumar-e@users.noreply.github.com>
2025-01-31 17:04:24 +0530

    Static Methods

--- basic/OOPs/Static/Static2.java
+++ basic/OOPs/Static/Static2.java
@@ -1,3 +1,17 @@
 public class Static2 {
     
+    public static void main(String[] args) {
+        A obj = new A();
+        obj.show();
+        A.display();
+    }
+}
+
+class A {
+    void show(){
+        System.out.println("Non-static method");
+    }
+    static void display(){
+        System.out.println("Static method");
+    }
 }
