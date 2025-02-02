9e0d7d0c5cf5c4dbf8c5bd680736e46749382d88
Arun Choudhary <98233935+Arun-kumar-e@users.noreply.github.com>
2025-02-02 15:21:47 +0530

    Different Return type

---
+++ basic/Assignment/OOPs/Runtime polymorphism/Q4.java
@@ -0,0 +1,20 @@
+public class Q4 {
+    public static void main(String[] args) {
+        B obj = new B();
+        obj.show();       
+    }
+}
+// Error because B doesn't have a same return type as A's show method
+class A {
+    int show(){
+        int a = 10;
+        System.out.println("Class A");
+        return a;
+    }
+}
+
+class B extends A {
+    void show(){
+        System.out.println("Class B");
+    }
+}
