ebc4453598ccb2fafc5618288362f1603a781d4f
Arun Choudhary <98233935+Arun-kumar-e@users.noreply.github.com>
2025-02-02 17:06:19 +0530

    Interface

---
+++ basic/Assignment/OOPs/Interface/Main.java
@@ -0,0 +1,36 @@
+interface A{
+    int sum(int a, int b);
+}
+interface B{
+    void show(String s);
+}
+interface C{
+    String show1();
+}
+interface D extends A,B,C{
+    void m4();
+}
+class E implements D{
+    public int sum(int a, int b){
+        return a+b;
+    }
+    public void show(String s){
+        System.out.println(s);
+    }
+    public String show1(){
+        return "Emilia Parez is a bad movie.";
+    }
+    public void m4(){
+        System.out.println("13 oscar nominations for what??");
+    }
+}
+
+public class Main{
+    public static void main(String []args){
+        E obj = new E();
+        System.out.println(obj.sum(10,15));
+        obj.show("Yo Boi!");
+        System.out.println(obj.show1());
+        obj.m4();
+    }
+}
