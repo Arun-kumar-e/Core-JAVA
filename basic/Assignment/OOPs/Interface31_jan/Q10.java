2675d871e06fe1721c272cced685291e2fcd5283
Arun Choudhary <98233935+Arun-kumar-e@users.noreply.github.com>
2025-02-07 21:26:46 +0530

    Static method

---
+++ basic/Assignment/OOPs/Interface31_jan/Q10.java
@@ -0,0 +1,10 @@
+class A{
+    static int Calculator(int a, int b){
+        return a * b;
+    }
+}
+public class Q10 {
+    public static void main(String[] args) {
+        System.out.println(A.Calculator(10, 5));
+    }
+}
