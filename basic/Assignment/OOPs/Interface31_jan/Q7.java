4c5cbc17e0c8b6c934873bec3fd1e443758b03cb
Arun Choudhary <98233935+Arun-kumar-e@users.noreply.github.com>
2025-02-07 21:12:11 +0530

    Lamba Expression

---
+++ basic/Assignment/OOPs/Interface31_jan/Q7.java
@@ -0,0 +1,15 @@
+interface Calculator {
+    int operate(int a, int b);
+}
+public class Q7 {
+    public static void main(String[] args) {
+        Calculator add = (a, b) -> a + b;
+        Calculator subtract = (a, b) -> a - b;
+        Calculator multiply = (a, b) -> a * b;
+        Calculator divide = (a, b) -> a / b;
+        System.out.println("10 + 5 = " + add.operate(10, 5));
+        System.out.println("10 - 5 = " + subtract.operate(10, 5));
+        System.out.println("10 * 5 = " + multiply.operate(10, 5));
+        System.out.println("10 / 5 = " + divide.operate(10, 5));
+    }
+}
