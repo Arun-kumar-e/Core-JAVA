936556770b9d159c6194dd0d3bfe658865568e75
Arun Choudhary <98233935+Arun-kumar-e@users.noreply.github.com>
2025-02-08 10:44:45 +0530

    Abstract class

---
+++ basic/Test/08-02-2025/Q4.java
@@ -0,0 +1,23 @@
+interface Shape {
+    abstract void calculateArea();
+}
+
+class Circle implements Shape {
+    public void calculateArea() {
+        System.out.println("Area of Circle");
+    }
+}
+
+class Rectangle implements Shape {
+    public void calculateArea() {
+        System.out.println("Area of Rectangle");
+    }
+}
+public class Q4 {
+    public static void main(String[] args) {
+        Circle c = new Circle();
+        Rectangle r = new Rectangle();
+        c.calculateArea();
+        r.calculateArea();
+    }
+}
