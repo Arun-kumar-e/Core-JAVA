23464c5b3d355dbc822aaf4084f32065fd02c9d1
Arun Choudhary <98233935+Arun-kumar-e@users.noreply.github.com>
2025-02-10 11:06:09 +0530

    Arrya

--- basic/Arrays/ArrayBasic2.java
+++ basic/Arrays/ArrayBasic2.java
@@ -1,3 +1,8 @@
 public class ArrayBasic2 {
-    
+    public static void main(String[] args) {
+        int a[] = { 1, 2, 3, 4, 5, 6 };
+        for (int i = a.length-1 ; i >= 0 ; i--){
+            System.out.print(a[i] + " ");
+        } 
+    }
 }
