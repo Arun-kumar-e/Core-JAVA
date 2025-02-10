506ca4200f2c7efce7011e949e732f691ccf6f53
Arun Choudhary <98233935+Arun-kumar-e@users.noreply.github.com>
2025-02-10 10:54:40 +0530

    printing array

--- basic/Arrays/ArrayBasic.java
+++ basic/Arrays/ArrayBasic.java
@@ -2,5 +2,11 @@ public class ArrayBasic {
     public static void main(String [] args){
         int a[] = { 1, 2, 3, 4, 5, 6 };
         System.out.println(a[2]);
+
+        // another way of printing array 
+
+        for (int i = 0 ; i <a.length ; i++){
+            System.out.print(a[i] +" ");
+        }
     }
 }
