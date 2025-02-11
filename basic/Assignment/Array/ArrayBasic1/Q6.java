77fb47042d1bf928e966b2027cf1334637ae11c2
Arun Choudhary <98233935+Arun-kumar-e@users.noreply.github.com>
2025-02-11 12:29:11 +0530

    Array print

---
+++ basic/Assignment/Array/ArrayBasic1/Q6.java
@@ -0,0 +1,7 @@
+public class Q6 {
+    public static void main(String[] args) {
+        int a[] = {12,67,46,435,67,385,335,335,34};
+
+        System.out.println("first element " +a[0] + "last Element " + a[a.length -1]);
+    }
+}
