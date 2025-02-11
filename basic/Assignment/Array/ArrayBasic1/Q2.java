9d70f003d8fae99ae0b907a29a20867666ff34e7
Arun Choudhary <98233935+Arun-kumar-e@users.noreply.github.com>
2025-02-11 12:20:32 +0530

    Different type array

---
+++ basic/Assignment/Array/ArrayBasic1/Q2.java
@@ -0,0 +1,29 @@
+public class Q2 {
+
+    public static void main(String[] args) {
+
+        char a[] = {'A','B','C','D','E'};
+        String b[] = {"Hello "," World "," Java "," Laptop"," Array "};
+        Float c[] = {55.5f,454.4f,454.55f,655.55f,454.02f,55.44f};
+        
+        for(int i = 0 ; i < a.length ; i++){
+                    System.out.print(a[i]);
+                }
+
+        System.out.println();
+    
+        for(int i = 0 ; i < b.length ; i++){
+                    System.out.print(b[i]);
+                }
+
+        System.out.println();
+    
+        for(int i = 0 ; i < c.length ; i++){
+                    System.out.print(c[i]);
+                }
+
+        System.out.println();
+
+    }
+}
+
