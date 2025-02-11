adca327c51004546ac3328aedc86a6f563c4f260
Arun Choudhary <98233935+Arun-kumar-e@users.noreply.github.com>
2025-02-11 12:27:33 +0530

    Arrray print

---
+++ basic/Assignment/Array/ArrayBasic1/Q5.java
@@ -0,0 +1,7 @@
+public class Q5 {
+    public static void main(String[] args) {
+        int a[] = {12,67,46,435,67,385,335,335,34};
+        int b = a.length/2;
+        System.out.println(a[b]);
+    }
+}
