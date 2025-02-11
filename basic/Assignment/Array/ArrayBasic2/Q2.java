68e93479fc2e2648d5dad24ff8039409cb712914
Arun Choudhary <98233935+Arun-kumar-e@users.noreply.github.com>
2025-02-11 14:42:07 +0530

    user input

---
+++ basic/Assignment/Array/ArrayBasic2/Q2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Q2 {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void userin(char a[]){
+        for(int i = 0 ; i<a.length ; i++){
+            a[i] = sc.next().charAt(0);
+        }
+    }
+
+    public static void printn(char a[]){
+        for(int i = 0 ; i< a.length ; i++){
+           System.out.print(a[i] + " ");
+        }
+   }
+
+    public static void main(String[] args) {
+        char a[] = new char[4]; 
+        userin(a);
+        printn(a);
+    }
+}
