5963078fe5aab9ccca5030936bbe25cb77988f80
Arun Choudhary <98233935+Arun-kumar-e@users.noreply.github.com>
2025-02-12 10:41:13 +0530

    shallow copy

---
+++ basic/Arrays/ArrayBasic8.java
@@ -0,0 +1,22 @@
+public class ArrayBasic8 {
+    
+    public static void printn(int a[]){
+        for(int i = 0 ; i< a.length ; i++){
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        
+        int a[] = {1,2,3,4,5,6};
+        
+        int b[] = a;
+
+        a[0] = 0;
+        a[1] = 0;
+
+        printn(a);
+        printn(b);
+    }
+}
