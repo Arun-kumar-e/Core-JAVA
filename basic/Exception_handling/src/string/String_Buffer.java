4ca123373b6b8564593ccc89b9205d9695331561
Arun Choudhary <98233935+Arun-kumar-e@users.noreply.github.com>
2025-03-10 11:32:52 +0530

    String Buffer

---
+++ basic/Exception_handling/src/string/String_Buffer.java
@@ -0,0 +1,11 @@
+package string;
+
+public class String_Buffer {
+
+	public static void main(String[] args) {
+		
+		StringBuffer sb = new StringBuffer("Arun");
+		sb.append(" Choudhary");
+		System.out.println(sb);
+	}
+}
