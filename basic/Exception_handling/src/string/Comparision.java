755ddfcac2e954951845d3c6053585f0a5b2d484
Arun Choudhary <98233935+Arun-kumar-e@users.noreply.github.com>
2025-03-10 11:32:19 +0530

    Difference between "==" and .equal() method

---
+++ basic/Exception_handling/src/string/Comparision.java
@@ -0,0 +1,14 @@
+package string;
+
+public class Comparision {
+
+	public static void main(String[] args) {
+		
+//    	String s1 = new String ("Java");
+		String s1 = "Java";
+		String s2 = "Java";
+		
+		System.out.println(s1 == s2);
+		System.out.println(s1.equals(s2));
+	}
+}
