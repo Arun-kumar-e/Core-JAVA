4cf8fd4c076a4fc7281af76b8f3820282b1cc20c
Arun Choudhary <98233935+Arun-kumar-e@users.noreply.github.com>
2025-03-10 11:31:26 +0530

    String Creation with different Ways

---
+++ basic/Exception_handling/src/string/String_Ceration.java
@@ -0,0 +1,18 @@
+package string;
+
+public class String_Ceration {
+
+	public static void main(String[] args) {
+		
+		String s = "Arun";
+		System.out.println(s);
+		
+//		System.out.println("***************");
+//		String s1 = new String("Java");
+//		System.out.println(s1);
+		
+		s= s.concat(" Choudhary");
+		System.out.println(s);
+		
+	}
+}
