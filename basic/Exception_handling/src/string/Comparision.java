d977190b4114a35aebf68fb323033fc37b061135
Arun Choudhary <98233935+Arun-kumar-e@users.noreply.github.com>
2025-03-19 11:04:07 +0530

    Comparision

--- basic/Exception_handling/src/string/Comparision.java
+++ basic/Exception_handling/src/string/Comparision.java
@@ -4,8 +4,8 @@ public class Comparision {
 
 	public static void main(String[] args) {
 		
-//    	String s1 = new String ("Java");
-		String s1 = "Java";
+    	String s1 = new String ("Java");
+//		String s1 = "Java";
 		String s2 = "Java";
 		
 		System.out.println(s1 == s2);
